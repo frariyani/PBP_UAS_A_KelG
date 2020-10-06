80c8d353a2c4b7b86e76a0c58c942a5c14991ea7
Calvindo <50109685+ryune12@users.noreply.github.com>
2020-10-06 14:10:14 +0700

    membuat layout dan menghubungkannya dengan fragment

---
+++ app/src/main/java/com/calvindo/aldi/sutanto/tubes/MainActivity.java
@@ -0,0 +1,53 @@
+package com.calvindo.aldi.sutanto.tubes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.navigation_view);
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selected = null;
+
+                    switch (item.getItemId()){
+                        case R.id.action_home:
+                            selected = new HomeFragment();
+                            break;
+                        case R.id.action_fav:
+                            selected = new FavoriteFragment();
+                            break;
+                        case R.id.action_profile:
+                            selected = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment,
+                            selected).commit();
+                    return true;
+                }
+            };
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        super.onCreateOptionsMenu(menu);
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//
+//        return true;
+//    }
+}
---
+++ app/src/main/java/com/calvindo/aldi/sutanto/tubes/models/Kost.java
@@ -0,0 +1,10 @@
+package com.calvindo.aldi.sutanto.tubes.models;
+
+public class Kost {
+
+    private String name;
+    private String alamat;
+    private String koordinat;
+    
+
+}

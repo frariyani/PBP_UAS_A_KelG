9801bafd64373e8b999a7a5eeef1c58abf837b6e
Ariyani <ariyani.kz@gmail.com>
2020-10-10 17:51:11 +0700

    tambah google service json

--- app/src/main/java/com/calvindo/aldi/sutanto/tubes/ProfileFragment.java
+++ app/src/main/java/com/calvindo/aldi/sutanto/tubes/ProfileFragment.java
@@ -1,17 +1,27 @@
 package com.calvindo.aldi.sutanto.tubes;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.BindingAdapter;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +32,7 @@ import android.widget.TextView;
 import com.calvindo.aldi.sutanto.tubes.databinding.FragmentProfileBinding;
 import com.calvindo.aldi.sutanto.tubes.models.User;
 import com.calvindo.aldi.sutanto.tubes.models.UserDummy;
+import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -30,21 +41,36 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+import static android.app.Activity.RESULT_OK;
+
 public class ProfileFragment extends Fragment{
-    public static final int REQUEST_CODE = 1;
+    private static final int CAMERA_REQUEST_CODE = 100;
+    private static final int STORAGE_REQUEST_CODE = 200;
+    private static final int IMAGE_PICK_GALLERY_CODE = 300;
+    private static final int IMAGE_PICK_CAMERA_CODE = 400;
+
+    private String cameraPermissions[];
+    private String storagePermissions[];
+
     //firebase
     private FirebaseAuth auth;
     private FirebaseUser user;
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
+    private StorageReference storageReference;
 
     //profile
     private ImageView avatarIv;
     private TextView namaTv, usernameTv, emailTv, notelpTv, alamatTv;
+    private MaterialButton logoutBtn;
+
+    //upload avatar
+    private Uri PickedImgUri;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -69,6 +95,154 @@ public class ProfileFragment extends Fragment{
         notelpTv = view.findViewById(R.id.notelpView);
         alamatTv = view.findViewById(R.id.alamatView);
 
+        //init array permission
+        cameraPermissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        getDataFromFirebase();
+
+        logoutBtn = view.findViewById(R.id.logout_button);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.signOut();
+            }
+        });
+
+        avatarIv = view.findViewById(R.id.avatarView);
+        avatarIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showUploadOptions();
+            }
+        });
+
+        return view;
+    }
+
+    private boolean checkStoragePermission(){
+        boolean result = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                         == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void requestStoragePermission(){
+        ActivityCompat.requestPermissions(getActivity(), storagePermissions, STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkCameraPermission(){
+        boolean resultCamera = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
+                            == (PackageManager.PERMISSION_GRANTED);
+        boolean resultWrite = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            == (PackageManager.PERMISSION_GRANTED);
+        return resultCamera && resultWrite;
+    }
+
+    private void requestCameraPermission(){
+        ActivityCompat.requestPermissions(getActivity(), cameraPermissions, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case CAMERA_REQUEST_CODE:{
+                if(grantResults.length > 0){
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if(cameraAccepted && writeAccepted){
+                        openCamera();
+                    }
+                }
+            }
+            case STORAGE_REQUEST_CODE:{
+                if(grantResults.length > 0){
+                    boolean writeAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if(writeAccepted){
+                        openGallery();
+                    }
+                }
+            }
+        }
+    }
+
+    private void openCamera(){
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Description");
+        PickedImgUri = getActivity().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        //start camera
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, PickedImgUri);
+        startActivityForResult(cameraIntent, IMAGE_PICK_CAMERA_CODE);
+    }
+
+    private void openGallery(){
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, IMAGE_PICK_GALLERY_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == IMAGE_PICK_GALLERY_CODE){
+                PickedImgUri = data.getData();
+            }
+            if(requestCode == IMAGE_PICK_CAMERA_CODE){
+
+            }
+        }
+    }
+
+    private void showUploadOptions(){
+        String options[] = {"Camera", "Gallery"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle("Pick Image From");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(i == 0){
+                    //upload lewat kamera
+                    if(!checkCameraPermission()){
+                        requestCameraPermission();
+                    }else{
+                        openCamera();
+                    }
+                }else if(i == 1){
+                    //upload lewat gallery
+                    if(!checkStoragePermission()){
+                        requestStoragePermission();
+                    }else{
+                        openGallery();
+                    }
+                }
+            }
+        });
+
+        builder.create().show();
+    }
+
+    //private void openGallery(){
+    //    Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+    //    galleryIntent.setType("image/*");
+    //    startActivityForResult(galleryIntent, GALLERY_REQUEST);
+    //}
+
+    //@BindingAdapter("bind:loadDrawable")
+    //public static void loadDrawable(ImageView imageView, String imageURL) {
+    //    int drawableResourceId = imageView.getResources().getIdentifier(imageURL, "drawable", imageView.getContext().getPackageName());
+    //    Log.i("tag","profile.id:" + drawableResourceId);
+    //    imageView.setImageResource(drawableResourceId);
+    //}
+
+    private void getDataFromFirebase(){
         Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
         query.addValueEventListener(new ValueEventListener() {
             @Override
@@ -96,28 +270,5 @@ public class ProfileFragment extends Fragment{
 
             }
         });
-
-        avatarIv = view.findViewById(R.id.avatarView);
-        avatarIv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        return view;
     }
-
-    private void openGallery(){
-        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        galleryIntent.setType("image/*");
-        startActivityForResult(galleryIntent, REQUEST_CODE);
-    }
-
-    //@BindingAdapter("bind:loadDrawable")
-    //public static void loadDrawable(ImageView imageView, String imageURL) {
-    //    int drawableResourceId = imageView.getResources().getIdentifier(imageURL, "drawable", imageView.getContext().getPackageName());
-    //    Log.i("tag","profile.id:" + drawableResourceId);
-    //    imageView.setImageResource(drawableResourceId);
-    //}
 }
---
+++ app/src/main/java/com/calvindo/aldi/sutanto/tubes/RegisterActivity.java
@@ -0,0 +1,154 @@
+package com.calvindo.aldi.sutanto.tubes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.calvindo.aldi.sutanto.tubes.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+    private TextInputEditText editEmail, editPassword, editNama, editUsername, editNoTelp, editAlamat;
+    private MaterialButton registerBtn;
+    private FirebaseAuth auth;
+    private FirebaseDatabase rootNode;
+    private DatabaseReference reference;
+    private String getEmail, getPassword;
+    private TextView loginLink;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //init edit text
+        editEmail = findViewById(R.id.input_email);
+        editPassword = findViewById(R.id.input_password);
+        editNama = findViewById(R.id.input_nama);
+        editUsername = findViewById(R.id.input_username);
+        editNoTelp = findViewById(R.id.input_notelp);
+        editAlamat = findViewById(R.id.input_alamat);
+
+        auth = FirebaseAuth.getInstance();
+
+        editPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+        registerBtn = findViewById(R.id.register_button);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isDataValid()){
+                    createUserAccount();
+                }
+            }
+        });
+
+        loginLink = findViewById(R.id.login_link);
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+    private boolean isDataValid(){
+        getEmail = editEmail.getText().toString();
+        getPassword = editPassword.getText().toString();
+
+        if(TextUtils.isEmpty(getEmail) && TextUtils.isEmpty(getPassword)){
+            Toast.makeText(this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+        }else if(!isEmailValid(getEmail)){
+            Toast.makeText(this, "Email Invalid", Toast.LENGTH_SHORT).show();
+        }else if(TextUtils.isEmpty(getPassword)){
+            Toast.makeText(this,"Please Enter Password", Toast.LENGTH_SHORT).show();
+        }else if(getPassword.length() < 6){
+            Toast.makeText(this, "Password Too Short", Toast.LENGTH_SHORT).show();
+        }else{
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isEmailValid(String email){
+        if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private void createUserAccount(){
+        getEmail = editEmail.getText().toString();
+        getPassword = editPassword.getText().toString();
+        auth.createUserWithEmailAndPassword(getEmail, getPassword)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_SHORT).show();
+                            //rootNode = FirebaseDatabase.getInstance();
+                            //reference = rootNode.getReference("User");
+
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String uid = firebaseUser.getUid();
+
+                            HashMap<Object, String> hashMap = new HashMap<>();
+
+                            hashMap.put("uid", uid);
+                            hashMap.put("email",  editEmail.getText().toString());
+                            hashMap.put("username", editUsername.getText().toString());
+                            hashMap.put("notelp", editNoTelp.getText().toString());
+                            hashMap.put("alamat", editAlamat.getText().toString());
+                            hashMap.put("nama", editNama.getText().toString());
+                            hashMap.put("avatar", ""); //input di edit profile
+
+                            //store hashmap ke database
+                            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                            DatabaseReference reference = firebaseDatabase.getReference("User");
+                            reference.child(uid).setValue(hashMap);
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Register Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    /*private void loginUserAccount() {
+        getEmail = editEmail.getText().toString();
+        getPassword = editPassword.getText().toString();
+        auth.signInWithEmailAndPassword(getEmail, getPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }*/
+}

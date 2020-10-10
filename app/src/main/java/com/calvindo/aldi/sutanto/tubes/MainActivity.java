e3b56d699811036dadcfb4e9a7dcdc99a8cb922d
CrisYustiantoPT <chris321.yusti@gmail.com>
2020-10-10 21:45:51 +0700

    Landscape and Potrait

--- app/src/main/java/com/calvindo/aldi/sutanto/tubes/MainActivity.java
+++ app/src/main/java/com/calvindo/aldi/sutanto/tubes/MainActivity.java
@@ -2,22 +2,35 @@ package com.calvindo.aldi.sutanto.tubes;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import static android.os.Build.VERSION_CODES.O;
+
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences preferences;
     public static final int mode = Activity.MODE_PRIVATE;
+    Button favorit;
+    private String CHANNEL_ID = "Channe 1";
 
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth auth;
@@ -55,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         //set default layout dengan homeFragment
         Fragment default_frag = new HomeFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment,
@@ -76,6 +91,52 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+        favorit = (Button) findViewById(R.id.btn_fav);
+        favorit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNotification();
+                addNotification();
+            }
+
+            private void createNotification() {
+                if(Build.VERSION.SDK_INT >= O){
+                    CharSequence name = "Channel 1";
+                    String description = "This is Channel 1";
+                    int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                    NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+                    channel.setDescription(description);
+
+                    //notification for System after add kost
+                    NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                    notificationManager.createNotificationChannel(channel);
+                }
+            }
+
+            private  void addNotification(){
+                //Menambhakan Notif saat menambah favorit kost.
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("BErhasil")
+                        .setContentText("Selamat kost anda berhasil ditambahkan")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                //Membuat Intent yang menampilkan Notification
+                Intent notificationIntent = new Intent(this,MainActivity.class);
+                PendingIntent contentIntent = PendingIntent.getActivity(this,0,notificationIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(contentIntent);
+
+                //Menampilkamn Notifikasi
+                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(0,builder.build());
+
+            }
+
+
+
+        });
+
+
 
     }
 
